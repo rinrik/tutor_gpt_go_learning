3aa6ee13548617d174d4ca477d318e972b4ca3c7
Rinat Khissamov <rinat.khissamov@agcsoft.com>
2024-12-04 16:10:08 +0200

    prepare functionality

---
+++ task_16_file_processing_with_gorutines/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var fileTexts [][]byte
+	files, _ := os.ReadDir("./files")
+	fmt.Println(files[0].Name())
+	for _, v := range files {
+		temp, _ := os.ReadFile("files/" + v.Name())
+		fileTexts = append(fileTexts, temp)
+
+	}
+
+	for _, f := range fileTexts {
+		fmt.Println(string(f))
+	}
+	// fmt.Println(fileTexts)
+}
